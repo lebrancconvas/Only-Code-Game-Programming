09518cc30b04e3a5716f4aaf11aed0f2a85ded13
lebrancconvas <lebrancconvas@gmail.com>
2021-10-16 05:00:31 +0700

    :sparkles: Draft Tic-Tac-Toe.

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 
-namespace Tic_Tac_Toe
+namespace Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			String[] Board = new String[] {".",".",".",
+																		 ".",".",".",
+																		 ".",".","."}; 
+			bool isConsidered = true;
+			int player = 1;
+			while(isConsidered)
+			{
+				if(player % 2 != 0)
+				{
+					Console.WriteLine("Player 1 Input: ");
+					String posIndex = Console.ReadLine();
+					int position = int.Parse(posIndex);
+					if(Board[position - 1] == ".")
+					{
+						Board[position - 1] = "x";
+					}
+					player++;
+					Console.WriteLine(String.Join(",", Board));
+				}
+
+				else
+				{
+					Console.WriteLine("Player 2 Input: ");
+					String posIndex = Console.ReadLine();
+					int position = int.Parse(posIndex);
+					if(Board[position - 1] == ".")
+					{
+						Board[position - 1] = "o";
+					}
+					player++;
+					Console.WriteLine(String.Join(",", Board));
+				}
+			}
+		}
+
+		static Boolean winCondition()
+		{
+			
+			return true;
+		}
+	}
+} 
